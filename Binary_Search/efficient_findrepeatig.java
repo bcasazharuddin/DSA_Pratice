4a1162d45e5c9be197f129ac002582b05b53fcb9
bcasazharuddin <bcasazharuddin@gmail.com>
2023-09-22 00:14:15 +0530

    first repeating value

---
+++ Binary_Search/efficient_findrepeatig.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    public static void main(String[] args) {
+
+        // Taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking total count of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // taking total count of elements
+            int n = sc.nextInt();
+
+            // creating a new array of size n
+            int arr[] = new int[n];
+
+            // inserting elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            // calling firstRepeated method
+            // and printing the result
+            System.out.println(ob.firstRepeated(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to return the position of the first repeating element.
+    public static int firstRepeated(int[] arr, int n) {
+        // Your code here
+        
+       int slow = arr[0];
+       int fast = arr[0];
+       
+       do{
+           
+           slow =   arr[slow];
+           fast = arr[arr[fast]];
+           
+       }while(slow != fast);
+       
+       slow = arr[0];
+       
+       while(slow != fast){
+           slow = arr[slow];
+           fast = arr[fast];
+       }
+       
+       return slow;
+    }
+    
+}
